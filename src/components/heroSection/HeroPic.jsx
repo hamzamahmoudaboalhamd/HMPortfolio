cd4c1885206b8b3c7937b65e548d1fcc0d1d77a1
Hamza Mahmoud <aboaihamdhamzamahmoud123@gmail.com>
2025-08-04 15:11:38 +0300

    Modifications

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -12,7 +12,7 @@ const HeroPic = () => {
       className=" h-full flex items-center justify-center "
     >
       <img
-        src="/HMPortfolio/images/HexaPic.png"
+        src="/images/HexaPic.png"
         alt="Hamza Mahmoud"
         className="max-h-[600px] w-auto"
       />
