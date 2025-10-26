13f9855bd7507448d70ab61f65a2c980e0ecdb9a
Hamza Mahmoud <aboaihamdhamzamahmoud123@gmail.com>
2025-10-26 22:54:46 +0300

    Modification

--- src/components/experienceSection/ExperienceTopLeft.jsx
+++ src/components/experienceSection/ExperienceTopLeft.jsx
@@ -9,7 +9,7 @@ const ExperienceTopLeft = () => {
       <div className="flex justify-center items-center gap-4">
         <ExperienceInfo number="2" text="Years" />
         <p className="font-bold text-6xl text-lightBrown">-</p>
-        <ExperienceInfo number="20" text="Websites" />
+        <ExperienceInfo number="23" text="Websites" />
       </div>
       <p className="text-center">
         With 2 years of experience building dynamic and user-friendly web
